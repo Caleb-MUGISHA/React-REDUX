ca9d2baf6e26014484ed04cbfb64ac5590fd4a91
Caleb-LeGrand12 <c.mugisha@alustudent.com>
2021-06-09 02:12:44 +0200

    almost done

--- partB/react-redux-demo/src/App.js
+++ partB/react-redux-demo/src/App.js
@@ -3,12 +3,19 @@ import HooksCakeContainer from "./component/HooksCakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import HooksIceCreamContainer from "./component/HooksIceCreamContainer";
+import NewCakeContainer from "./component/NewCakeContainer";
+import ItemContainer from "./component/ItemContainer";
+import UserContainer from "./component/UserContainer";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <UserContainer />
+        {/* <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <HooksIceCreamContainer />
+        <NewCakeContainer /> */}
       </div>
     </Provider>
   );
